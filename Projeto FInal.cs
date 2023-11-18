a9b6e95fea6419038e2214be7f03f987eb6d53ad
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2023-11-17 22:11:28 -0300

    dreamteam

--- Projeto FInal.cs
+++ Projeto FInal.cs
@@ -11,31 +11,60 @@ Console.WriteLine("Top 20 MELHORES jogadores do mundo:");
 Console.WriteLine("+===================================+" );
 Console.WriteLine("|  TOP |      NOME      |  POSIÇÃO  |" );
 Console.WriteLine("+===================================+" );
-Console.WriteLine("|  1   | Michael Jordan |  Armador  | ");
-Console.WriteLine("|  2   |  LeBron James  |    Ala    |");
-Console.WriteLine("|  3   |  Kareem Abdul  |    Pivô   |");
-Console.WriteLine("|  4   |  Magic Johnson |  Armador  |");
-Console.WriteLine("|  5   |Wilt Chamberlain|    Pivô   |");
-Console.WriteLine("|  6   |  Bill Russell  |    Pivô   |");
-Console.WriteLine("|  7   |    Larry Bird  |    Ala    |");
-Console.WriteLine("|  8   |   Tim Duncan   |  Ala-Pivô |");
-Console.WriteLine("|  9   | Oscar Robertson|  Armador  |");
-Console.WriteLine("|  10  |   Kobe Bryant  |  Armador  |");
-Console.WriteLine("|  11  |Shaquille O\'Neal|    Pivô   |");
-Console.WriteLine("|  12  |  Kevin Durant  |    Ala    |");
-Console.WriteLine("|  13  | Hakeem Olajuwon|    Pivô   |");
-Console.WriteLine("|  14  |  Julius Erving |    Ala    |");
-Console.WriteLine("|  15  |  Moses Malone  |    Pivô   |");
-Console.WriteLine("|  16  |  Stephen Curry |  Armador  |");
-Console.WriteLine("|  17  |  Dirk Nowitzki |  Ala-Pivô |");
-Console.WriteLine("|  18  |   Giannis A.   |  Ala-Pivô |");
-Console.WriteLine("|  19  |   Jerry West   |  Armador  |");
-Console.WriteLine("|  20  |   Elgin Baylor |    Ala    |");
+Console.WriteLine("|  #1  | Michael Jordan |  Armador  | ");
+Console.WriteLine("|  #2  |  LeBron James  |    Ala    |");
+Console.WriteLine("|  #3  |  Kareem Abdul  |    Pivô   |");
+Console.WriteLine("|  #4  |  Magic Johnson |  Armador  |");
+Console.WriteLine("|  #5  |Wilt Chamberlain|    Pivô   |");
+Console.WriteLine("|  #6  |  Bill Russell  |    Pivô   |");
+Console.WriteLine("|  #7  |    Larry Bird  |    Ala    |");
+Console.WriteLine("|  #8  |   Tim Duncan   |  Ala-Pivô |");
+Console.WriteLine("|  #9  | Oscar Robertson|  Armador  |");
+Console.WriteLine("| #10  |   Kobe Bryant  |  Armador  |");
+Console.WriteLine("| #11  |Shaquille O\'Neal|    Pivô   |");
+Console.WriteLine("| #12  |  Kevin Durant  |    Ala    |");
+Console.WriteLine("| #13  | Hakeem Olajuwon|    Pivô   |");
+Console.WriteLine("| #14  |  Julius Erving |    Ala    |");
+Console.WriteLine("| #15  |  Moses Malone  |    Pivô   |");
+Console.WriteLine("| #16  |  Stephen Curry |  Armador  |");
+Console.WriteLine("| #17  |  Dirk Nowitzki |  Ala-Pivô |");
+Console.WriteLine("| #18  |   Giannis A.   |  Ala-Pivô |");
+Console.WriteLine("| #19  |   Jerry West   |  Armador  |");
+Console.WriteLine("| #20  |   Elgin Baylor |    Ala    |");
 Console.WriteLine("+===================================+");
+Console.WriteLine(" ");
 
+double Armador, Pivo1, Pivo2, AlaD, AlaE;
 
-  
 Thread.Sleep(5000);
 
 Console.WriteLine("Adicione um jogador escolhendo o número dele");
-Console.WriteLine("0 jogador(es) selecionados.");
+Console.WriteLine("0 jogadores selecionados.");
+Console.WriteLine(" ");
+
+Console.Write("Digite o número de um Armador: ");
+Armador = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("1 jogador(es) foram selecionados.");
+Console.WriteLine(" ");
+
+Console.Write("Digite o número da Ala Direita: ");
+AlaD = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("2 jogador(es) foram selecionados.");
+Console.WriteLine(" ");
+
+Console.Write("Digite o número da Ala Esquerda: ");
+AlaE = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("3 jogador(es) foram selecionados.");
+Console.WriteLine(" ");
+
+Console.Write("Digite o número do Pivô 1: ");
+Pivo1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("4 jogador(es) foram selecionados.");
+Console.WriteLine(" ");
+
+Console.Write("Digite o número do Pivô 2: ");
+Pivo2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("5 jogador(es) foram selecionados.");
+
+Console.WriteLine("Seu time é:");
+
