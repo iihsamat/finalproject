ee4766a4a886f1bf22d368e179dbb4d3b6e07c18
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2023-11-17 21:36:41 -0300

    dreamteam

---
+++ Projeto FInal.cs
@@ -0,0 +1,41 @@
+﻿//Stefeson
+
+Console.Clear();
+
+Console.WriteLine("Forme seu time de basquete dos sonhos");
+
+Thread.Sleep(2000);
+
+Console.WriteLine("Top 20 MELHORES jogadores do mundo:");
+
+Console.WriteLine("+===================================+" );
+Console.WriteLine("|  TOP |      NOME      |  POSIÇÃO  |" );
+Console.WriteLine("+===================================+" );
+Console.WriteLine("|  1   | Michael Jordan |  Armador  | ");
+Console.WriteLine("|  2   |  LeBron James  |    Ala    |");
+Console.WriteLine("|  3   |  Kareem Abdul  |    Pivô   |");
+Console.WriteLine("|  4   |  Magic Johnson |  Armador  |");
+Console.WriteLine("|  5   |Wilt Chamberlain|    Pivô   |");
+Console.WriteLine("|  6   |  Bill Russell  |    Pivô   |");
+Console.WriteLine("|  7   |    Larry Bird  |    Ala    |");
+Console.WriteLine("|  8   |   Tim Duncan   |  Ala-Pivô |");
+Console.WriteLine("|  9   | Oscar Robertson|  Armador  |");
+Console.WriteLine("|  10  |   Kobe Bryant  |  Armador  |");
+Console.WriteLine("|  11  |Shaquille O\'Neal|    Pivô   |");
+Console.WriteLine("|  12  |  Kevin Durant  |    Ala    |");
+Console.WriteLine("|  13  | Hakeem Olajuwon|    Pivô   |");
+Console.WriteLine("|  14  |  Julius Erving |    Ala    |");
+Console.WriteLine("|  15  |  Moses Malone  |    Pivô   |");
+Console.WriteLine("|  16  |  Stephen Curry |  Armador  |");
+Console.WriteLine("|  17  |  Dirk Nowitzki |  Ala-Pivô |");
+Console.WriteLine("|  18  |   Giannis A.   |  Ala-Pivô |");
+Console.WriteLine("|  19  |   Jerry West   |  Armador  |");
+Console.WriteLine("|  20  |   Elgin Baylor |    Ala    |");
+Console.WriteLine("+===================================+");
+
+
+  
+Thread.Sleep(5000);
+
+Console.WriteLine("Adicione um jogador escolhendo o número dele");
+Console.WriteLine("0 jogador(es) selecionados.");
